2bf2a8b01c08526295633b1abf9e435095b9e085
Naveen <naveen.kaushik.21@mountblue.tech>
2024-10-03 00:28:20 +0530

    setup redux store

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const user = useSelector((store) => store.user);
+  console.log(user);
+
   return (
     <div className="navbar bg-base-300">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">devTinder</a>
-        </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">devTinder</a>
+      </div>
+      {user && (
         <div className="flex-none gap-2">
-          <div className="form-control">
-            {/* <input
-              type="text"
-              placeholder="Search"
-              className="input input-bordered w-24 md:w-auto"
-            /> */}
-          </div>
-          <div className="dropdown dropdown-end mx-4">
+          <div className="form-control">Welcome, {user.firstName}</div>
+
+          <div className="dropdown dropdown-end mx-5 flex">
             <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
+                <img alt="user photo" src={user.photoUrl} />
               </div>
             </div>
             <ul
@@ -46,8 +43,9 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-      </div>
-  )
-}
+      )}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
